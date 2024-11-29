6d37d8cedf0bf74ef1e2fb1476903f8fd17c99ee
kanAfridi <kafridi281@gmail.com>
2024-11-30 01:14:16 +0500

    Final steps

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 
 import streamlit as st
 import pandas as pd
-import pickle 
+#import pickle 
 import numpy as np
 from utils import load_data, Tv_show_similarity
+from api.api import fetch_image_path
 
-# Load the dataframe
+# Load the dataframe and saved array
 df = load_data()
-
-# Load the saved array
 similarity_score = Tv_show_similarity()
 
-
+# Function to recommend similar TV shows based on tv id
 def show_similarity(series):
     try:
         series_index = df[df['name'] == series].index[0]
@@ -19,9 +18,13 @@ def show_similarity(series):
         five_similar = sorted(list(enumerate(similarity)), reverse=True, key= lambda x: x[1])[1:6]
 
         recommended_tv_shows = []
+        recommended_posters = []
         for i, score in five_similar:
+            #
+            tv_ids = df.loc[i]['id']
             recommended_tv_shows.append(df.loc[i]['name'])
-        return recommended_tv_shows
+            recommended_posters.append(fetch_image_path(tv_ids))
+        return recommended_tv_shows, recommended_posters
             
     except FileNotFoundError as e:
         print(f"Error: {e}")
@@ -34,7 +37,29 @@ st.title("Tv Shows Reocommending Model")
 selected_tvshow = st.selectbox("Select a TV Show", df['name'])
 
 if st.button('Recommendation'):
-        similar_tvshow = show_similarity(selected_tvshow)
-        for i in similar_tvshow:
-            st.write(i)
+        names, posters = show_similarity(selected_tvshow)
+        for i in names:
+            #st.write(i)
+
+            col1, col2, col3, col4, col5 = st.columns(5)
+
+        with col1:
+            st.markdown(f"#### {names[0]}")  # Use H3 for smaller text
+            st.image(posters[0])
+
+        with col2:
+            st.markdown(f"#### {names[1]}")  # Use H3 for smaller text
+            st.image(posters[1])
+
+        with col3:
+            st.markdown(f"#### {names[2]}")  # Use H3 for smaller text
+            st.image(posters[2])
+
+        with col4:
+            st.markdown(f"#### {names[3]}")  # Use H3 for smaller text
+            st.image(posters[3])
+
+        with col5:
+            st.markdown(f"#### {names[4]}")  # Use H3 for smaller text
+            st.image(posters[4])
 
